f4f341cc68ac8c7f32cc537e1a2689d66da741cc
william s <w.schloetel@protonmail.com>
2023-01-06 12:41:50 -0800

    updated .gitignore

--- tests/test_christofides.py
+++ tests/test_christofides.py
@@ -3,14 +3,11 @@ import christofides as ch
 import pytest
 
 
-# num_random_graphs = 10
-# num_nodes = 8
-# min_edge_weight = 400
-# max_edge_weight = 900
+
 
 @pytest.fixture
 def random_graph():
-	"""Generates random graph with 10 nodes using utils.random_random_graph"""
+	"""Generates random graph with 10 nodes using utils.random_graph"""
 	return r.generate_random_graph(10, 100, 1000)
 
 def test_path_has_correct_num_nodes(random_graph):
---
+++ tests/test_kruskals_algorithm.py
@@ -0,0 +1,5 @@
+import utils.random_graph as r
+import kruskals_algorithm as kr
+import pytest
+
+
